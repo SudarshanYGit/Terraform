d68444cb0660f1f573cb7ec9470ab7c25aaa75df
[SudarshanYGit] <[sudarshanryelwande@gmail.com]>
2025-12-27 13:43:58 +0530

    Completed

--- EC2_instance/main.tf
+++ EC2_instance/main.tf
@@ -1,8 +1,6 @@
 provider "aws" {
   region = var.aws_region
 }
-
-# 1. Security Group (Allow SSH & HTTP)
 resource "aws_security_group" "ec2_sg" {
   name        = "ec2_sg"
   description = "Allow SSH and HTTP"
@@ -32,8 +30,6 @@ resource "aws_security_group" "ec2_sg" {
     Name = "EC2-SG"
   }
 }
-
-# 2. Launch First EC2 Instance
 resource "aws_instance" "first" {
   ami                    = var.base_ami
   instance_type          = var.instance_type
@@ -44,16 +40,12 @@ resource "aws_instance" "first" {
     Name = "First-EC2"
   }
 }
-
-# 3. Create AMI from First Instance
 resource "aws_ami_from_instance" "first_ami" {
   name               = "first-ec2-ami"
   source_instance_id = aws_instance.first.id
 
   depends_on = [aws_instance.first]
 }
-
-# 4. Launch Second EC2 Instance from AMI
 resource "aws_instance" "second" {
   ami                    = aws_ami_from_instance.first_ami.id
   instance_type          = var.instance_type
