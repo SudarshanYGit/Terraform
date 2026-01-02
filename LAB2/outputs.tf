a3e87a6d7078b3bed92df5a18f2234388bd74d91
[SudarshanYGit] <[sudarshanryelwande@gmail.com]>
2026-01-02 12:47:19 +0530

    Added outputs.tf

--- LAB2/outputs.tf
+++ LAB2/outputs.tf
@@ -4,16 +4,16 @@ output "vpc_id" {
 }
 
 output "public_subnet_id" {
-  description = "ID of the public subnet"
+  description = "ID of the public-subnet"
   value       = aws_subnet.public.id
 }
 
 output "private_subnet_id" {
-  description = "ID of the private subnet"
+  description = "ID of the private-subnet"
   value       = aws_subnet.private.id
 }
 
 output "security_group_id" {
-  description = "ID of the security group"
+  description = "ID of the security-group"
   value       = aws_security_group.demo_sg.id
 }
