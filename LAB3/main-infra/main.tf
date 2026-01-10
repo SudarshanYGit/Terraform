bf8c8539004ffaceb414bb4a025de3e0fa3fb025
SudarshanYGit <sudarshanryelwande@gmail.com>
2026-01-10 23:31:35 +0530

    Made changes in the main setup

--- LAB3/main-infra/main.tf
+++ LAB3/main-infra/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "tf-state-Sudarshan-1072"
-    key            = "env/dev/terraform.tfstate"  # folder/path inside bucket
+    key            = "env/dev/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "tf-state-locks-Sudarshan-1072"
     encrypt        = true
